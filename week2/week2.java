15a57ea32c73b24d28ab9f7479efbaa388287a63
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2024-05-07 14:44:43 +0900

    Add files via upload

---
+++ week2/week2.java
@@ -0,0 +1,44 @@
+2. .java 파일이 실행되는 과정을 설명하시오.
+
+3. 변수의 종류에는 인스턴스 변수,클래스 변수, 지역 변수가 있다. 각 변수의 스코프와 라이프 타임에 대해 설명하시오.
+        
+4. 콘텍스트 스위칭이 일어나는 상황과 과정을 설명하시오. (Process p1과 Process p2가 있다고 가정)
+
+5. 3항 연산자가 무엇인지 예시를 들어 설명하시오.
+
+6. A는 Object를 상속받고 B는 A를 상속받는다고 가정했을때, 다음 코드의 실행 결과는 무엇인가?
+        Object o = new Object();
+        A a = new A();
+        B b = new B();
+     if(b instanceof A)
+         System.out.print("b is instanceof A")
+     if(b instanceof Object)
+        System.out.print("b is instanceof Obj")
+     if(a instanceof Object)
+        System.out.print("b is instanceof Obj")
+     if(o instanceof A)
+        System.out.print("obj is instanceof A")
+     if(o instanceof B)
+        System.out.print("obj is instanceof B")
+     if(a instanceof B)
+        System.out.print("a is instanceof B")
+
+7. 빈칸에 알맞은 말을 넣으시오
+        : 스왑 아웃, 스왑 인을 통해 메모리에 로드되는 프로세스 수를 동적으로 조절한다.
+        : 준비 큐에 어떤 프로세스를 넣을지 결정해 메모리에 올라가는 프로세스 수를 조절한다.
+        : 준비큐에 있는 대기 프로세스 중 어떤 프로세를 다음으로 실행할지 결정한다.
+
+8. 비선점형 스케줄링은 무엇이고 방식 2가지와 선점형 스케줄링은 무엇이고 방식 3가지를 적으시오.
+
+9. 내부,외부 단편화란?
+
+10. 최초 적합,최악 적합이란?
+
+11. 비연속 메모리 할당 방법 2가지
+
+12. 시간,공간적 지역성이란?
+
+13. 요구페이징이 어떻게 동작하는지 설명하시오.
+
+14. 비 좀 그만 왔으면..
+
